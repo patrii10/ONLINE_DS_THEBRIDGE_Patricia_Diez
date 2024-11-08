330866db7ca7f04c0cb170938e15c331d94ea71d
patrii10 <patriciadiezromero@gmail.com>
2024-11-08 17:33:33 +0100

    Create variables_patri.py

---
+++ Sprint_04/ejercicios/variables_patri.py
@@ -0,0 +1,14 @@
+#Dimension de lado del tablero
+TABLERO_LONGITUD = 10
+
+#Caracter para posición de agua sin disparo
+CARACTER_AGUA = " "
+
+#Caracter para posición de barco sin disparo
+CARACTER_BARCO = " "
+
+#Caracter para posición de barco con disparo
+CARACTER_DISPARO_OK = "*"
+
+#Caracter para posición agua con disparo
+CARACTER_DISPARO_NOK = "-"
